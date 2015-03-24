c1f54b20a0218e28feec607df2f22893c99d6baf
shivali-batra <shivalib@intelligrape.com>
2015-03-24 16:42:33 +0530

    updated rating

--- grails-app/controllers/com/ig/LinkShare/showPostController.groovy
+++ grails-app/controllers/com/ig/LinkShare/showPostController.groovy
@@ -20,11 +20,10 @@ class showPostController {
         User currentUser = userService.showCurrentUserObject(session["username"])
 
         Subscription subscription = Subscription.findWhere(user: currentUser)
-        println("........"+resource)
-         float averageRating=resourceRatingService.findAverageRating(resource)
-//        println averageRating
 
-        render(view: "/showPost/viewPost", model: [averageRating:averageRating,loginUser: currentUser, subscription: subscription, trendingTopicList: trendingTopics, resource: resource])
+        float averageRating = resourceRatingService.findAverageRating(resource)
+
+        render(view: "/showPost/viewPost", model: [averageRating: averageRating, loginUser: currentUser, subscription: subscription, trendingTopicList: trendingTopics, resource: resource])
     }
 
     def postsForAdmin() {
@@ -34,27 +33,24 @@ class showPostController {
 
         List<Topic> trendingTopics = trendingTopicService.showTrendingTopics()
 
-//        def averageRating=resourceRatingService.findAverageRating()
-
         render(view: "/showPost/viewPost", model: [loginUser: currentUser, trendingTopicList: trendingTopics, resourceList: resourceList])
     }
 
     def rateResource() {
-        println "............... in resource rate : "
         String scr = params.rating
-        Float score=scr.toDouble()
+        Float score = scr.toDouble()
         User currentUser = userService.showCurrentUserObject(session["username"])
 
         ResourceRating resourceRating1 = ResourceRating.findByUser(currentUser)
 
         if (resourceRating1) {
             resourceRating1.score = score
-            resourceRating1.save(failOnError: true,flush: true)
+            resourceRating1.save(failOnError: true, flush: true)
         } else {
             ResourceRating resourceRating = new ResourceRating(user: currentUser, resource: params.resourceID, score: score)
             resourceRating.save(failOnError: true, flush: true)
         }
 
-     render true
+        render true
     }
 }
