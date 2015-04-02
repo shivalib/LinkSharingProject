a48aae82a089b37561427cef7e943a4f380d7874
shivali-batra <shivalib@intelligrape.com>
2015-04-02 13:37:59 +0530

    updated postRating.js to show ratings for all post of admin correctly

--- grails-app/assets/javascripts/postRating.js
+++ grails-app/assets/javascripts/postRating.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
 
 
-    var avgRate = $('.ratyDiv').attr('data-avg')
     $('.ratyDiv').each(function () {
-            $(this).raty({
-                score: avgRate,
-                click: function (score) {
-                    var url = $(this).attr('data-rateLink')
-                    var id = $(this).attr('data-resourceID')
-                    $.ajax({
-                        url: url,
-                        data: {
-                            resourceID: id,
-                            rating: score
-                        },
-                        success: function (data) {
-                            console.log(data)
-                        },
-                        error: function (request, status, error) {
-                            console.log("We are in error section ");
-                            console.log("request :" + request);
-                            console.log("status : " + status);
-                            console.log("error :" + error);
-                        }
+        var avgRate = $(this).attr('data-avg')
+        $(this).raty({
+            score: avgRate,
+            click: function (score) {
+                var url = $(this).attr('data-rateLink')
+                var id = $(this).attr('data-resourceID')
+                $.ajax({
+                    url: url,
+                    data: {
+                        resourceID: id,
+                        rating: score
+                    },
+                    success: function (data) {
+                        console.log(data)
+                    },
+                    error: function (request, status, error) {
+                        console.log("We are in error section ");
+                        console.log("request :" + request);
+                        console.log("status : " + status);
+                        console.log("error :" + error);
+                    }
 
-                    });
-                },
-                half: true,
-                showHalf: true,
-                starHalf: '/assets/star_half.png',
-                starOn: '/assets/star_on.png',
-                starOff: '/assets/star_off.png'
-            });
+                });
+            },
+            half: true,
+            showHalf: true,
+            starHalf: '/assets/star_half.png',
+            starOn: '/assets/star_on.png',
+            starOff: '/assets/star_off.png'
+        });
     });
 });
 
