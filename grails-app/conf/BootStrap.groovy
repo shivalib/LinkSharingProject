09480105356cd5c458db54c449b68bc04a886428
shivali-batra <shivalib@intelligrape.com>
2015-04-09 11:17:46 +0530

    removed all bootstrap code

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,11 +4,6 @@ import com.ig.LinkShare.applicationEnums.GenerateToken
 class BootStrap {
 
     def init = { servletContext ->
-        GenerateToken generateToken = new GenerateToken()
-        List<User> userList=User.list()
-        userList.each {User user->
-            generateToken.generateTokenForRegisteredUser(user)
-        }
     }
 
     def destroy = {
