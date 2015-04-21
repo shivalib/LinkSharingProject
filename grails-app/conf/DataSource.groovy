9b16e7a436267112a5edca8cdfacedfadbe036ff
shivali-batra <shivalib@intelligrape.com>
2015-04-21 16:26:16 +0530

    updated DataSource.groovy : set state to update

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -18,8 +18,8 @@ hibernate {
 environments {
     development {
         dataSource {
-//            dbCreate="update"
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate="update"
+//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://127.0.0.1:3306/LinkShare?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8& zeroDateTimeBehavior = convertToNull"
 
             //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
@@ -32,7 +32,7 @@ environments {
             username="shivali-batra"
             password="igdefault"
             pooled="true"
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost:3306/shivali-batra_QA"
         }
     }
